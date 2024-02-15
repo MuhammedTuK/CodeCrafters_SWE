146220ee03d1fc2d78b673f64a005694c94fde11
Mohammed Aldawsari <mxal3h@rit.edu>
2024-02-14 21:05:24 -0500

    prject example inital commit

--- CourseExamples/ProjectExamples/ExampleProject/PageAPI.php
+++ CourseExamples/ProjectExamples/ExampleProject/PageAPI.php
@@ -10,46 +10,76 @@ $jsonData = get_file_by_id($_GET["id"]);
 
 
 // -------------- Start paragraph gathering information 
-function getParagraphInfo1() {
+function getParagraphInfo1()
+{
 	return readJSONFile("inputFiles/para1.json");
 }
 
-function getParagraphInfo2() {
+function getParagraphInfo2()
+{
 	return readJSONFile("inputFiles/para2.json");
 }
 
-function getParagraphInfo3() {
+function getParagraphInfo3()
+{
 	return readJSONFile("inputFiles/para3.json");
 }
 
-function getParagraphInfo4() {
+function getParagraphInfo4()
+{
 	return readJSONFile("inputFiles/para4.json");
 }
 
-function getParagraphInfo5() {
+function getParagraphInfo5()
+{
 	return readJSONFile("inputFiles/para5.json");
 }
 
+function getParagraphInfo6()
+{
+	return readJSONFile("inputFiles/para6.json");
+}
 
-function getParagraphInformation($rand){
-//$rand = 1;
-	switch ($rand){
-	    case "1":
-	      $jsonData = getParagraphInfo1();
-	      break;
-	    case "2":
-	      $jsonData = getParagraphInfo2();
-	      break;
-	    case "3":
-	      $jsonData = getParagraphInfo3();
-	      break;
-	    case "4":
-	      $jsonData = getParagraphInfo4();
-	      break;
-	    case "5":
-	      $jsonData = getParagraphInfo5();
-	      break;
-	  }
+function getParagraphInfo7()
+{
+	return readJSONFile("inputFiles/para7.json");
+}
+
+function getParagraphInfo8()
+{
+	return readJSONFile("inputFiles/para8.json");
+}
+
+
+function getParagraphInformation($rand)
+{
+	//$rand = 1;
+	switch ($rand) {
+		case "1":
+			$jsonData = getParagraphInfo1();
+			break;
+		case "2":
+			$jsonData = getParagraphInfo2();
+			break;
+		case "3":
+			$jsonData = getParagraphInfo3();
+			break;
+		case "4":
+			$jsonData = getParagraphInfo4();
+			break;
+		case "5":
+			$jsonData = getParagraphInfo5();
+			break;
+		case "6":
+			$jsonData = getParagraphInfo6();
+			break;
+		case "7":
+			$jsonData = getParagraphInfo7();
+			break;
+		case "8":
+			$jsonData = getParagraphInfo8();
+			break;
+	}
 }
 
 
@@ -59,53 +89,73 @@ function getParagraphInformation($rand){
 
 // Call the JSON files using these functions.
 //		This could likely be removed and the file calls made directly in the if statement.
-function getDropDownInformationState() {
+function getDropDownInformationState()
+{
 	return readJSONFile("inputFiles/dropDownListState.json");
 }
 
-function getDropDownInformationStooge() {
+function getDropDownInformationStooge()
+{
 	return readJSONFile("inputFiles/dropDownListStooge.json");
 }
 
-function getDropDownInformationMLB() {
+function getDropDownInformationMLB()
+{
 	return readJSONFile("inputFiles/dropDownListBaseball.json");
 }
 
-function getDropDownInformationNFL() {
+function getDropDownInformationNFL()
+{
 	return readJSONFile("inputFiles/dropDownListNFL.json");
 }
 
-function getDropDownInformationNHL() {
+function getDropDownInformationNHL()
+{
 	return readJSONFile("inputFiles/dropDownListNHL.json");
 }
 
+function getDropDownInformationSNames()
+{
+	return readJSONFile("inputFiles/dropDownListStudents.json");
+}
 
-function getMultChoiceInformation() {
+function getListInformationTeams()
+{
+	return readJSONFile("inputFiles/ListTeams.json");
+}
+
+function getMultChoiceInformation()
+{
 	return readJSONFile("inputFiles/shortOptions.json");
 }
 
 
 
 // If it is a dropdown list, take the random value that is being passed in to randomly determine what the contents of the dropdown list should be. This will help with dynamically creating the page.
-function getDropDownInformation($rand){
-//$rand = 1;
-	switch ($rand){
-	    case "1":
-	      $jsonData = getDropDownInformationState();
-	      break;
-	    case "2":
-	      $jsonData = getDropDownInformationStooge();
-	      break;
-	    case "3":
-	      $jsonData = getDropDownInformationMLB();
-	      break;
-	    case "4":
-	      $jsonData = getDropDownInformationNFL();
-	      break;
-	    case "5":
-	      $jsonData = getDropDownInformationNHL();
-	      break;
-	  }
+function getDropDownInformation($rand)
+{
+	//$rand = 1;
+	switch ($rand) {
+		case "1":
+			$jsonData = getDropDownInformationState();
+			break;
+		case "2":
+			$jsonData = getDropDownInformationStooge();
+			break;
+		case "3":
+			$jsonData = getDropDownInformationMLB();
+			break;
+		case "4":
+			$jsonData = getDropDownInformationNFL();
+			break;
+		case "5":
+			$jsonData = getDropDownInformationSNames();
+			break;
+		case "6":
+			$jsonData = getDropDownInformationNHL();
+		case "7":
+			$jsonData = getListInformationTeams();
+	}
 }
 
 
@@ -116,7 +166,8 @@ function getDropDownInformation($rand){
 // ----------------------- THE PRIMARY PAGE FUNCTIONS -----------------------
 
 // Create a basic file for just getting the contents of a JSON file
-function readJSONFile($url) {
+function readJSONFile($url)
+{
 
 	$JSON = file_get_contents($url);
 	echo $JSON; // return the information to be read by the reader page
@@ -124,33 +175,33 @@ function readJSONFile($url) {
 
 
 // Get a random value from an external API.
-function getRandomValue(){
+function getRandomValue()
+{
 
-// Make this a variable
+	// Make this a variable
 	// https://l3harriscourseexamples.onrender.com/ProjectExamples/RandomAPI.php?action=get_RandomValue
-	$extRandomURL = "https://courseexamplespage.onrender.com/CourseExamples/ProjectExamples/RandomAPI.php?action=get_RandomValue"; // make this a global variable
+	$extRandomURL = "https://l3harriscourseexamples.onrender.com/ProjectExamples/RandomAPI.php?action=get_RandomValue"; // make this a global variable
 	$content = file_get_contents($extRandomURL);
 	$result  = json_decode($content);
 	return $result;
 }
 
 // The main function that drives the page
-function get_file_by_id($id){
-	$rand = getRandomValue();
- 	switch ($id){
-    case "dropdown":
-      $jsonData = getDropDownInformation($rand);
-      break;
-    case "paragraphInfo":
-      $jsonData = getParagraphInformation($rand);
-      break;
-    case "multChoice":
-      $jsonData = getMultChoiceInformation();
-      break;
-  	}
+function get_file_by_id($id)
+{
+	//$rand = getRandomValue();
+	$jsonDataSel = $_GET["sel"];
+	$static_SNames = 5;
+	$static_teams = $jsonDataSel;
+	switch ($id) {
+		case "dropdown":
+			$jsonData = getDropDownInformation($static_SNames);
+			break;
+		case "paragraphInfo":
+			$jsonData = getParagraphInformation($static_teams);
+			break;
+		case "multChoice":
+			$jsonData = getMultChoiceInformation();
+			break;
+	}
 }
-
-
-?>
-
-
