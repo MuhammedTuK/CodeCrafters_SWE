a453dac552bb1d20cdf08e615c7c5da7a950ebff
Mohammed Aldawsari <m.aldossari@gmail.com>
2024-02-19 19:19:10 -0500

    initial commit for user story #2&3&4&5

--- API_Example/PageAPI.php
+++ API_Example/PageAPI.php
@@ -3,6 +3,18 @@
 // The purpose of this API is to act as an intermediary to determing the json information to be returned by the example PageReader page.
 $jsonData = get_file_by_id($_GET["id"]);
 
+function readJSONFile($url)
+{
+	$JSON = file_get_contents($url);
+	echo $JSON; // return the information to be read by the reader page
+}
+
+function readImgFile($url)
+{
+	$img = file_get_contents($url);
+	echo $img; // return the information to be read by the reader page
+}
+
 //  Start dropdown gathering information 
 function getDropDownInformationSNames()
 {
@@ -12,8 +24,37 @@ function getListInformationTeams()
 {
 	return readJSONFile("inputFiles/ListTeams.json");
 }
+function getPhotoInformation($sel)
+{
+	switch($sel){
+		case "1":
+			return readImgFile("inputFiles/abdulatif.jpg");
+			break;
+		case "2":
+			return readImgFile("inputFiles/dawsari.jpg");
+			break;
+		case "3":
+			return readImgFile("inputFiles/johani.jpg");
+			break;
+		case "4":
+			return readImgFile("inputFiles/otaibi.jpg");
+			break;
+		case "5":
+			return readImgFile("inputFiles/shahrani.jpg");
+			break;
+		case "6":
+			return readImgFile("inputFiles/zhrani.jpg");
+			break;
+		case "7":
+			return readImgFile("inputFiles/tukhays.jpg");
+			break;
+		case "8":
+			return readImgFile("inputFiles/ghadi.jpg");
+			break;
+	}
+}
 
-// If it is a dropdown list, take the random value that is being passed in to randomly determine what the contents of the dropdown list should be. This will help with dynamically creating the page.
+// If it is a dropdown list, take the sel value determine what the contents of the dropdown list should be
 function getDropDownInformation($sel)
 {
 	switch ($sel) {
@@ -27,25 +68,21 @@ function getDropDownInformation($sel)
 }
 
 // ----------------------- THE PRIMARY PAGE FUNCTIONS -----------------------
-// Create a basic file for just getting the contents of a JSON file
-function readJSONFile($url)
-{
-	$JSON = file_get_contents($url);
-	echo $JSON; // return the information to be read by the reader page
-}
 
 // The main function that drives the page
 function get_file_by_id($id)
 {
-	$static_SNames = 1;
-	$static_Teams = 2;
+	$sel = $_GET["selected"];
 	
 	switch ($id) {
 		case "dropdown":
-			$jsonData = getDropDownInformation($static_SNames);
+			$jsonData = getDropDownInformation(1);
 			break;
 		case "paragraphInfo":
-			$jsonData = getDropDownInformation($static_Teams);
+			$jsonData = getDropDownInformation(2);
+			break;
+		case "photoInfo":
+			$jsonData = getPhotoInformation($sel);
 			break;
 	}
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 L3Harris Software Engineering course Repo for CodeCrafters team.
 
-The hosted versions of this code are located on [Render](https://codecrafters-webservice.onrender.com). Code from this repository is automatically deployed once it is committed to this repository.
+The hosted versions of this code are located on [Render](https://codecrafters-webpage.onrender.com). Code from this repository is automatically deployed once it is committed to this repository.
